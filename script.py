8d351ae10b8fb628d4a98e2c40ec6784e03baea3
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-11-26 10:18:37 -0800

    added db connection

--- script.py
+++ script.py
@@ -4,6 +4,10 @@
 # The Crucible
 
 import argparse
+import sys
+import pandas as pd
+from pymongo import MongoClient, errors
+
 
 # Helper methods
 def timecode(frame, fps):
@@ -17,5 +21,31 @@ def timecode(frame, fps):
     return f"{hours:02}:{minutes:02}:{seconds:02}:{frames:02}"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--baselight", "-bl", action="store_true", help="input baselight file")
-parser.add_argument("--xytech", "-xt",action="store_true", help="input xytech file")
+parser.add_argument("--baselight", "-bl", action="store", help="input baselight file")
+parser.add_argument("--xytech", "-xt",action="store", help="input xytech file")
+
+args = parser.parse_args()
+
+# Connect to DB
+client = MongoClient('localhost', 27017)
+database = client["crucible"]
+
+if args.baselight:
+    collection_name = "baselight"
+    collection = database[collection_name]
+    try:
+        baselight_file = open(args.baselight, "r")
+        baselight_lines = baselight_file.readlines()
+
+        for line in baselight_lines:
+            each_line = line.strip().split()
+            url = each_line[0].strip("/baselightfilesystem1")
+            frames = list(map(int, each_line[1:]))
+            collection.insert_one({"url": url, "frames": frames})
+    except FileNotFoundError:
+        print(f"{args.baselight} file not found")
+        sys.exit(1)
+    except errors.ConnectionFailure:
+        print("Could not connect to MongoDB")
+        sys.exit(1)
+
