06614dac4c32227b807f4ee2aadf3fbddbfa453c
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-12-17 18:28:03 -0800

    refactored methods

--- script.py
+++ script.py
@@ -23,6 +23,17 @@ parser.add_argument("--output", action="store_true", help="export data into XLS 
 
 args = parser.parse_args()
 
+# Set up vimeo client
+vimeo_client = vimeo.VimeoClient(
+    token = "0070400c399f5e91c87ad0e80960999c",
+    client_id='6b1666c5d4c95d3ecd5941b8dec63a7dab93112d',
+    client_secret='xUzuhK2OYRA2ogYXxZ8eAqouKylGckReL4LJyll4Wg/oXJ4b18bFQIZBY1y7uwjIm+SlMrQgm4VbG/Dg0So3NVV/Z4q0ESksXeFB/42hkH3D9foA9m/7TwAFv72rWsPg',
+)
+
+# Connect to DB
+client = MongoClient('localhost', 27017)
+database = client["crucible"]
+
 # Helper methods
 def timecode(frame, fps):
     frames = frame % fps
@@ -47,15 +58,67 @@ def find_timecode_range(frame):
     end = timecode(post_frame, fps)
     return f"{start}-{end}"
 
-vimeo_client = vimeo.VimeoClient(
-    token = "0070400c399f5e91c87ad0e80960999c",
-    client_id='6b1666c5d4c95d3ecd5941b8dec63a7dab93112d',
-    client_secret='xUzuhK2OYRA2ogYXxZ8eAqouKylGckReL4LJyll4Wg/oXJ4b18bFQIZBY1y7uwjIm+SlMrQgm4VbG/Dg0So3NVV/Z4q0ESksXeFB/42hkH3D9foA9m/7TwAFv72rWsPg',
-)
+def get_vimeo_data():
+    # Export data from Vimeo account to a csv
+    csv_path = "output/vimeo_data.csv"
+    with open(csv_path, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["Title", "URI", "Public Link", "Status"])
+        response = vimeo_client.get("/me/videos")
 
-# Connect to DB
-client = MongoClient('localhost', 27017)
-database = client["crucible"]
+        while True:
+            data = response.json()
+            for video in data["data"]:
+                title = video.get("name")
+                uri = video.get("uri")
+                link = video.get("link")
+                status = video.get("status")
+                writer.writerow([title, uri, link, status])
+
+            next_page = data.get("paging").get("next")
+            if not next_page:
+                break
+            response = vimeo_client.get(next_page)
+
+    print("Vimeo data exported to csv!")
+
+def create_thumbnail(frame):
+    timestamp = frame / fps
+    output_path = os.path.join(output_folder, f"{frame}_thumbnail.png")
+
+    (
+        ffmpeg
+        .input(args.process, ss=timestamp)
+        .output(output_path, vframes=1, s="96x74")
+        .overwrite_output()
+        .run()
+    )
+
+    return output_path
+
+def create_clip(frame):
+    # Create clip and upload to Vimeo
+    output_video_path = os.path.join(output_folder, f"{frame}.mp4")
+    start = (frame - 48) / fps
+    end = (frame + 48) / fps
+    duration = end - start
+    timecode_range = find_timecode_range(frame)
+
+    (
+        ffmpeg
+        .input(args.process, ss=start, t=duration)
+        .output(output_video_path)
+        .overwrite_output()
+        .run()
+    )
+
+    video = vimeo_client.upload(
+        output_video_path,
+        data={
+            "name": f"Clip for {timecode_range}"
+        }
+    )
+    print(f"Clip {timecode_range} created and uploaded successfully!")
 
 # Import baselight data to DB
 # Excludes preliminary baselight1 directory
@@ -108,9 +171,10 @@ if args.xytech:
 if args.process:
     baselight_db = database["baselight"]
     xytech_db = database["xytech"]
-    frames = []
+    results = []
     fps = 24
 
+    # Extract timecode from video
     video_data = ffmpeg.probe(args.process)
     for i in video_data["streams"]:
         if i["index"] == 0:
@@ -118,68 +182,47 @@ if args.process:
             print(f"Timecode extracted from {args.process} is {video_timecode}")
             print("*" * 40)
 
-    # Find all corresponding frames
+    # Find all data corresponding to video
     contents = baselight_db.find()
     for content in contents:
         folder = content["folder"]
-        if "Planeshifter" in folder:
-            frames.extend(content["frames"])
-
-if args.output:
-    rows = []
-
-    for content in baselight_db.find():
-        folder = content["folder"]
-        frames = content["frames"]
+        if "Planeshifter"  not in folder:
+            continue
 
         workorder, location = find_new_url(folder, xytech_db)
         if workorder is None or location is None:
             continue
 
-        for frame in frames:
-            # Obtain thumbnail
-            timestamp = frame / fps
-            output_folder = "output"
-            output_path = os.path.join(output_folder, f"{frame}_thumbnail.png")
-            timecode_range = find_timecode_range(frame)
-
-            # export thumbnail
-            (
-                ffmpeg
-                .input(args.process, ss=timestamp)
-                .output(output_path, vframes=1, s="96x74")
-                .overwrite_output()
-                .run()
-            )
-
-            # Create clip and upload to Vimeo
-            output_video_path = os.path.join(output_folder, f"{frame}.mp4")
-            start = (frame - 48) / fps
-            end = (frame + 48) / fps
-            duration = end - start
-
-            (
-                ffmpeg
-                .input(args.process, ss=start, t=duration)
-                .output(output_video_path)
-                .overwrite_output()
-                .run()
-            )
-
-            video = vimeo_client.upload(
-                output_video_path,
-                data={
-                    "name": f"Clip for {timecode_range}"
-                }
-            )
-
-            rows.append({
-                "Workorder": workorder,
-                "Location": location,
-                "Frame Range": f"{frame - 48} - {frame + 48}",
-                "Timecode Range": timecode_range,
-                "Thumbnail": output_path,
+        for frame in content["frames"]:
+            results.append({
+                "workorder": workorder,
+                "location": location,
+                "frame": frame,
+                "frame range": f"{frame - 48} - {frame + 48}",
+                "timecode range": find_timecode_range(frame),
             })
+
+if args.output:
+    rows = []
+    output_folder = "output"
+
+    for result in results:
+        frame = result["frame"]
+        workorder = result["workorder"]
+        location = result["location"]
+        timecode_range = result["timecode range"]
+
+        thumbnail = create_thumbnail(frame)
+
+        rows.append({
+            "Workorder": workorder,
+            "Location": location,
+            "Frame Range": f"{frame - 48} - {frame + 48}",
+            "Timecode Range": timecode_range,
+            "Thumbnail": thumbnail,
+        })
+
+        create_clip(frame)
 
     df = pd.DataFrame(rows)
     df.to_excel("output/output.xlsx", index=False)
@@ -190,40 +233,17 @@ if args.output:
 
     thumbnails = df.columns.get_loc("Thumbnail") + 1
 
-    for row_idx in range(2, len(df) + 2):
-        img_path = ws.cell(row=row_idx, column=thumbnails).value
+    for row in range(2, len(df) + 2):
+        img_path = ws.cell(row=row, column=thumbnails).value
         if img_path and os.path.exists(img_path):
             img = Image(img_path)
             img.width = 96
             img.height = 74
-            ws.add_image(img, ws.cell(row=row_idx, column=thumbnails).coordinate)
-            ws.cell(row=row_idx, column=thumbnails).value = ""
+            ws.add_image(img, ws.cell(row=row, column=thumbnails).coordinate)
+            ws.cell(row=row, column=thumbnails).value = ""
 
     wb.save("output/output.xlsx")
-    print(f"Data exported to file!")
+    print(f"Data exported output/output.xlsx successfully!")
 
-    # Export data from Vimeo account to a csv
-    csv_path = "output/output.csv"
-    with open(csv_path, "w", newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(["Title", "URI", "Public Link", "Status"])
-
-        response = vimeo_client.get("/me/videos")
-
-        while True:
-            data = response.json()
-            for video in data["data"]:
-                title = video.get("name")
-                uri = video.get("uri")
-                link = video.get("link")
-                status = video.get("status")
-
-                writer.writerow([title, uri, link, status])
-
-            next_page = data.get("paging").get("next")
-            if not next_page:
-                break
-            response = vimeo_client.get(next_page)
-
-    print("Vimeo data exported to csv!")
+    get_vimeo_data()
 
